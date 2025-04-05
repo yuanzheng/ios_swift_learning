7b25ef97f42f09d336e49ad970cd97f36bebf6c9
Yuanzheng (Lucas) Song <song_yuanzheng@hotmail.com>
2025-04-05 11:38:56 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Master on 2025/4/5.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
